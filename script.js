77ab34d208a44642cd3e11a17566c81b85624400
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2024-06-14 00:49:08 -0500

    2nd day progress

---
+++ script.js
@@ -0,0 +1,34 @@
+class Calculator {
+  constructor(PreviousOperantTextElement, currebtOperantTextElement) {
+    this.previousOperandTextElement = previousOperantTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {
+
+  }
+  delete)() {
+
+  }
+
+  appendNumber()appendNumber {
+
+  }
+
+  chooseOperation() {
+    
+  }
+
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-equals]");
+const previousOperantTextElement = document.querySelector(
+  "[data- previous-operand]"
+);
+const currebtOperantTextElement = document.querySelector(
+  "[data-current-operand]"
+);
